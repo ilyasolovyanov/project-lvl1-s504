7b54cef1dc0e447614a1fa95193187a8535922ee
ilya <ilya@ilyas-MacBook-Air.local>
2019-05-30 15:03:50 +0300

    Update

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,7 +1,7 @@
 import { startGame, getRandomInt } from '..';
 
 const isPrime = (num) => {
-  for (let i = 2; i < num / 2; i += 1) if (num % i === 0) return false;
+  for (let i = 2; i <= num / 2; i += 1) if (num % i === 0) return false;
   return num > 1;
 };
 
